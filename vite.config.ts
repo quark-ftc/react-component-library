a3ccaba380c47a1521178561a9007f6ecd931eef
Fang Tiancong <2830923997@qq.com>
2024-09-08 21:40:31 +0800

    feat: 添加全局样式并重置样式
    - 安装sass-embedded作为开发依赖
    - 在_variables.scss中添加变量，并在vite.config.ts中作为additionalData引入
    - 赋值normalize.css到_reset.scss中，使用_variables.scss中的变量替换，在main.ts中导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import 'src/styles/variables.scss';`,
+      },
+    },
+  },
 });
