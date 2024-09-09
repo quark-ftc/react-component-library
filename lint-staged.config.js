2c8aaededddeedc2a91beb53c2aadae435ab9f73
Fang Tiancong <2830923997@qq.com>
2024-09-09 22:43:42 +0800

    chore(lint-staged.config.js): 修改lint-staged.config.js配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,12 @@
+const processFiles = (fileNames) => {
+  const joinedFileNames = fileNames.join(' ');
+  return [
+    `pnpm exec eslint ${joinedFileNames}`,
+    `pnpm exec prettier -w ${joinedFileNames}`,
+  ];
+};
+
 export default {
-  'src/**/*.{js,mjs,cjs,ts,jsx,tsx,css,scss,json},*.{ts,js,json,html}': (
-    fileNames,
-  ) => {
-    const joinedFileNames = fileNames.join(' ');
-    return [
-      `pnpm exec eslint ${joinedFileNames}`,
-      `pnpm exec prettier -w ${joinedFileNames}`,
-    ];
-  },
+  'src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx,css,scss}': processFiles,
+  './*.{js,mjs,cjs,ts,mts,cts,jsx,tsx,css,scss}': processFiles,
 };
